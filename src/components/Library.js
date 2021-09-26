2a3d33eedd176878a69adfbf0f75012f200bd908
neelxie <kidricederek@gmail.com>
2021-09-26 11:28:10 +0300

    Add new changes

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,39 +1,62 @@
 import React from "react";
 
 const Library = () => {
+  const deleteBookHandler = (id) => {
+    // delete dispatch
+  };
   return (
     <div>
       <h1>Library</h1>
       <table>
         <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Pages</th>
-            </tr>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Rating</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>The Hobbit</td>
-                <td>J.R.R. Tolkien</td>
-                <td>320</td>
-            </tr>
-            <tr>
-                <td>The Lord of the Rings</td>
-                <td>J.R.R. Tolkien</td>
-                <td>480</td>
-            </tr>
-            <tr>
-                <td>The Catcher in the Rye</td>
-                <td>J.D. Salinger</td>
-                <td>240</td>
-            </tr>
-            <tr>
-                <td>To Kill a Mockingbird</td>
-                <td>Harper Lee</td>
-                <td>320</td>
-            </tr>
-
+          <tr>
+            <td>The Hobbit</td>
+            <td>J.R.R. Tolkien</td>
+            <td>320</td>
+            <td>
+              <button className="delete" onClick={deleteBookHandler}>
+                x
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>The Lord of the Rings</td>
+            <td>J.R.R. Tolkien</td>
+            <td>480</td>
+            <td>
+              <button className="delete" onClick={deleteBookHandler}>
+                x
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>The Catcher in the Rye</td>
+            <td>J.D. Salinger</td>
+            <td>240</td>
+            <td>
+              <button className="delete" onClick={deleteBookHandler}>
+                x
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>To Kill a Mockingbird</td>
+            <td>Harper Lee</td>
+            <td>320</td>
+            <td>
+              <button className="delete" onClick={deleteBookHandler}>
+                x
+              </button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
